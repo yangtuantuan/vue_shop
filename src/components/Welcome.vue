550d6a0f3a33626a6173799cddecae48f001c327
yangtuantuan <yangchengtuan@126.com>
2022-04-02 21:16:53 +0800

    左菜单

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{title}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data() {
+    return {
+      title: 'Welcome'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
